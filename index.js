b35b33df2d04c274ceec306d87345721e2fb98af
Jason Park <jjpark987@gmail.com>
2022-10-13 10:19:57 -0700

    commit tree d1888812fe57750d5df89e5221e3f2adb3f058ec

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(initialBlock, finalBlock) {
+    return Math.abs(finalBlock - initialBlock) * 264;
+}
+
+function calculatesFarePrice (initialBlock, finalBlock) {
+    const distanceInFeet = distanceTravelledInFeet(initialBlock, finalBlock);
+    if (distanceInFeet < 400) {
+        return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet < 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+        return 25;
+    } else {
+        return `cannot travel that far`;
+    }
+}
